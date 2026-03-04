4101867238889e0944156a9c778fb8cc0e2fa33c
AnnaLiulkova <al644722@gmail.com>
2026-03-04 11:00:33 +0200

    fix: change thread termination logic

--- src/main/java/org/example/BreakThread.java
+++ src/main/java/org/example/BreakThread.java
@@ -2,26 +2,46 @@ package org.example;
 
 public class BreakThread implements Runnable{
 
-    private boolean[] canBreak;
+    private volatile boolean[] canBreak;
+    private final long[] stopTimes;
 
     public BreakThread(int threadsCount) {
         this.canBreak = new boolean[threadsCount];
+        this.stopTimes = new long[threadsCount];
+
+        System.out.println("Generated time for threads:");
+
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < threadsCount; i++) {
+            long delaySeconds = 2 + (long)(Math.random() * 7);
+            this.stopTimes[i] = startTime + (delaySeconds * 1000);
+            System.out.println((i + 1) + " thread - " + delaySeconds + " seconds");
+        }
+        System.out.println("---");
     }
 
     @Override
     public void run() {
-        for (int i = 0; i < canBreak.length; i++) {
+        int stoppedCount = 0;
+
+        while (stoppedCount < canBreak.length) {
+            long currentTime = System.currentTimeMillis();
+
+            for (int i = 0; i < canBreak.length; i++) {
+                if (!canBreak[i] && currentTime >= stopTimes[i]) {
+                    canBreak[i] = true;
+                    stoppedCount++;
+                }
+            }
             try {
-                long delay = 2000 + (long)(Math.random() * 2001);
-                Thread.sleep(delay);
+                Thread.sleep(1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            canBreak[i] = true;
         }
     }
 
-    synchronized boolean isCanBreak(int id) {
+    boolean isCanBreak(int id) {
         return canBreak[id];
     }
 }
